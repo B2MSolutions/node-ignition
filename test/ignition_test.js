44e93ae2358ac51dc41ca0ae5933be347495f52b
jamesbloomer and sjwood <development+jamesbloomer+sjwood@b2m-solutions.com>
2012-05-15 12:08:49 +0100

    wired up the start and stop methods

--- test/ignition_test.js
+++ test/ignition_test.js
@@ -1,48 +1,72 @@
 var should = require('should');
 var sinon = require('sinon');
+var mocha = require('mocha');
 
 var ignition = require('../lib/ignition.js');
 
 describe('ignition', function(){
-  before(function(){
-  });
-
-  describe('#getDisplayTextForState()', function(){
-    it('should return start when stopped', function(){
-			ignition.getDisplayTextForState('stopped', function(error, response) {
-				response.should.equal('start');
-			});
+    before(function(){
     });
 
-    it('should return start when stopping', function(){
-			ignition.getDisplayTextForState('stopping', function(error, response) {
-				response.should.equal('start');
-			});
+    describe('#getDisplayTextForState()', function(){
+        it('should return start when stopped', function(){
+    			ignition.getDisplayTextForState('stopped', function(error, response) {
+    				response.should.equal('start');
+    			});
+        });
+    
+        it('should return start when stopping', function(){
+    			ignition.getDisplayTextForState('stopping', function(error, response) {
+    				response.should.equal('start');
+    			});
+        });
+    
+    	it('should return stop when running', function(){
+    		ignition.getDisplayTextForState('running', function(error, response) {
+    			response.should.equal('stop');
+    		});
+        });
+    
+    	it('should return ? when shutting-down', function(){
+    		ignition.getDisplayTextForState('shutting-down', function(error, response) {
+    			response.should.equal('?');
+    		});
+        });
+    
+    	it('should return ? when terminated', function(){
+    		ignition.getDisplayTextForState('terminated', function(error, response) {
+    			response.should.equal('?');
+    		});
+        });
+    
+    	it('should return stop when pending', function(){
+    		ignition.getDisplayTextForState('pending', function(error, response) {
+    			response.should.equal('stop');
+    		});
+        });
     });
-
-		it('should return stop when running', function(){
-			ignition.getDisplayTextForState('running', function(error, response) {
-				response.should.equal('stop');
-			});
+    
+    describe('#start()', function(){
+        it('should call AWS2JS request function with parameter StartInstances', function() {
+            var stub = sinon.stub(ignition.ec2, 'request').yields(null, null);
+            ignition.start('TestInstance', function(error, response) {
+                should.not.exist(error);
+                stub.calledOnce.should.be.true;
+                stub.calledWith('StartInstances', { "InstanceId.0" : "TestInstance" } ).should.be.true;
+                ignition.ec2.request.restore();
+            }); 
+        });
     });
-
-		it('should return ? when shutting-down', function(){
-			ignition.getDisplayTextForState('shutting-down', function(error, response) {
-				response.should.equal('?');
-			});
+    
+    describe('#stop()', function(){
+        it('should call AWS2JS request function with parameter StopInstances', function() {
+            var stub = sinon.stub(ignition.ec2, 'request').yields(null, null);
+            ignition.stop('TestInstance', function(error, response) {
+                should.not.exist(error);
+                stub.calledOnce.should.be.true;
+                stub.calledWith('StopInstances', { "InstanceId.0" : "TestInstance" } ).should.be.true;
+                ignition.ec2.request.restore();
+            }); 
+        });
     });
-
-		it('should return ? when terminated', function(){
-			ignition.getDisplayTextForState('terminated', function(error, response) {
-				response.should.equal('?');
-			});
-    });
-
-		it('should return stop when pending', function(){
-			ignition.getDisplayTextForState('pending', function(error, response) {
-				response.should.equal('stop');
-			});
-    });
-
-  });
 });

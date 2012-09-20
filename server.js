1c433f39062bc16b7f30827be0b456cac2f5d373
roylines <roy@roylines.co.uk>
2012-09-20 14:08:54 +0100

    added cryptozoological authentication

--- server.js
+++ server.js
@@ -1,71 +1,113 @@
 var express = require('express'),
     fs = require('fs'),
-    util = require('util'),
-    cluster = require('cluster'),
-    numCPUs = require('os').cpus().length;
+    util = require('util');
 
 var ignition = require('./lib/ignition.js');
 
 var app = express.createServer();
 
-var port = 3000; // process.env.PORT; 
+var port = 3000; // process.env.PORT;
 
-if (cluster.isMaster) {
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on('death', function(worker) {
-    console.log('worker ' + worker.pid + ' died, restarting new worker');
-    cluster.fork();
-  });
-} else {
-  app.listen(port);
-}
+app.listen(port);
 
 var logFile = fs.createWriteStream('./ignition.log', {flags: 'a'});
+
+var started = [];
+
+var zoo = {
+    a: 'ant',
+    b: 'bat',
+    c: 'cat',
+    d: 'dog',
+    e: 'elephant',
+    f: 'frog',
+    g: 'giraffe',
+    h: 'horse',
+    i: 'iguana',
+    j: 'jaguar',
+    k: 'koala',
+    l: 'llama',
+    m: 'mouse',
+    n: 'newt',
+    o: 'octopus',
+    p: 'platypus',
+    q: 'quail',
+    r: 'rat',
+    s: 'snake',
+    t: 'tiger',
+    u: 'urial',
+    v: 'vole',
+    w: 'walrus',
+    x: 'xantis',
+    y: 'yak',
+    z: 'zebra'
+};
+
+var cryptozoologicalAuthentication = function(username, password) {
+    var cryptozoologicalName = zoo[username[0]] + zoo[username[1]] + zoo[username[2]];
+    console.log('cryptozoologicalAuthentication: ' + cryptozoologicalName);
+    var user = null;
+    if(password === cryptozoologicalName) {
+        user = { name: username };
+    }
+
+    return user;
+};
 
 app.configure(function() {
     app.set('view engine', 'jade');
     app.set('view options', { layout: false });
+    app.use(express.cookieParser());
     app.use(express.bodyParser());
+    app.use(express.session({ secret: 'volcanoes at the end of the world' }));
     app.use(express.logger({stream: logFile}));
     app.use("/css", express.static(__dirname + '/css'));
 });
-
 
 app.get('/login/:instanceId/:elasticIp', function(req, res) {
     res.render('logon', {instanceId : req.params.instanceId, elasticIp : req.params.elasticIp});
 });
 
-
 app.post('/login/:instanceId/:elasticIp', function(req, res) {
-    
-    // TODO real authentication
-    log(util.format('user %s logged in with password %s', req.body.user, req.body.password));
-    res.redirect('/show/' + req.params.instanceId + '/' + req.params.elasticIp);
+    console.log("logging in");
+    var username = req.body.username;
+    var password = req.body.password;
+    var user = cryptozoologicalAuthentication(username, password);
+
+    if(user) {
+        log(util.format('user %s logged in with password %s', username, password));
+        req.session.regenerate(function() {
+            req.session.user = user;
+            res.redirect('/show/' + req.params.instanceId + '/' + req.params.elasticIp);
+        });
+    } else {
+        res.redirect('/login/' + req.params.instanceId + '/' + req.params.elasticIp);
+    }
 });
 
-app.get('/show/:instanceId/:elasticIp?', function(req, res){
+app.get('/show/:instanceId/:elasticIp?', ensureAuthenticated, function(req, res){
     ignition.getState(req.params.instanceId, function(error, response) {
-		res.render('ignition', 
+		res.render('ignition',
             {
-                action : response.text, 
-                instanceId : req.params.instanceId, 
-                elasticIp : req.params.elasticIp, 
+                action : response.text,
+                instanceId : req.params.instanceId,
+                elasticIp : req.params.elasticIp,
                 currentState : response.currentState,
-                refresh : req.query.refresh
+                refresh : req.query.refresh,
+                startedby: started[req.params.instanceId]
             });
-	}); 
+	});
 });
 
-app.get('/start/:instanceId/:elasticIp', function(req, res, next){
-	ignition.start(req.params.instanceId, function(error, response) {
+app.get('/start/:instanceId/:elasticIp', ensureAuthenticated, function(req, res, next){
+    started[req.params.instanceId] = req.session.user.name;
+
+    ignition.start(req.params.instanceId, function(error, response) {
         (function poll(){
             setTimeout(function(){
                 ignition.associateElasticIp(req.params.instanceId, req.params.elasticIp, function(error, response) {
                     if (error === null) {
-                        log(util.format('started instance %s', req.params.instanceId));
+                        log(util.format('instance %s started by %s', req.params.instanceId, req.session.user.name));
                         return res.redirect('/show/' + req.params.instanceId + '/' + req.params.elasticIp);
                     } else {
                         poll();
@@ -76,7 +118,7 @@ app.get('/start/:instanceId/:elasticIp', function(req, res, next){
 	});
 });
 
-app.get('/stop/:instanceId/:elasticIp', function(req, res, next){
+app.get('/stop/:instanceId/:elasticIp', ensureAuthenticated, function(req, res, next){
 	ignition.stop(req.params.instanceId, function(error, response) {
         log(util.format('stopped instance %s', req.params.instanceId));
 		res.redirect('/show/' + req.params.instanceId + '/' + req.params.elasticIp);
@@ -87,4 +129,12 @@ function log(message) {
     var msg = util.format('\n%s : %s\n\n', new Date(Date.now()).toISOString(), message);
     console.log(msg);
     logFile.write(msg);
+}
+
+function ensureAuthenticated(req, res, next) {
+    if (req.session.user) {
+        return next();
+    }
+
+    res.redirect('/login/' + req.params.instanceId + '/' + req.params.elasticIp);
 }

7c37f3518a97ffe59957fdd069fcf9aeb565424d
roylines <roy@roylines.co.uk>
2012-11-21 14:35:58 +0000

    added tests for getInstanceDetails

--- test/ignition_test.js
+++ test/ignition_test.js
@@ -8,7 +8,7 @@ describe('ignition', function(){
     before(function(){
     });
 
-    describe('#getDisplayTextForState()', function(){
+    describe('#getDisplayTextForState()', function() {
         it('should return start when stopped', function() {
             ignition.getDisplayTextForState('stopped').should.equal('start');
         });
@@ -34,39 +34,123 @@ describe('ignition', function(){
         });
     });
     
-    describe('#start()', function(){
-        it('should call AWS2JS request function with parameter StartInstances', function() {
+    describe('#start()', function() {
+        it('should call AWS2JS request function with parameter StartInstances', function(callback) {
             var stub = sinon.stub(ignition.ec2, 'request').yields(null, null);
             ignition.start('TestInstance', function(error, response) {
                 should.not.exist(error);
                 stub.calledOnce.should.be.true;
                 stub.calledWith('StartInstances', { "InstanceId.0" : "TestInstance" } ).should.be.true;
                 ignition.ec2.request.restore();
+                callback();
             }); 
         });
     });
     
-    describe('#stop()', function(){
-        it('should call AWS2JS request function with parameter StopInstances', function() {
+    describe('#stop()', function() {
+        it('should call AWS2JS request function with parameter StopInstances', function(callback) {
             var stub = sinon.stub(ignition.ec2, 'request').yields(null, null);
             ignition.stop('TestInstance', function(error, response) {
                 should.not.exist(error);
                 stub.calledOnce.should.be.true;
                 stub.calledWith('StopInstances', { "InstanceId.0" : "TestInstance" } ).should.be.true;
                 ignition.ec2.request.restore();
+                callback();
             }); 
         });
     });
     
-    describe('#associateElasticIp()', function(){
-        it('should call AWS2JS request function with parameters AssociateAddress, the instanceId and the Elastic IP', function() {
+    describe('#associateElasticIp()', function() {
+        it('should call AWS2JS request function with parameters AssociateAddress, the instanceId and the Elastic IP', function(callback) {
             var stub = sinon.stub(ignition.ec2, 'request').yields(null, null);
             ignition.associateElasticIp('TestInstance', '0.0.0.0', function(error, response) {
                 should.not.exist(error);
                 stub.calledOnce.should.be.true;
                 stub.calledWith('AssociateAddress', { "InstanceId" : "TestInstance", "PublicIp" : "0.0.0.0" } ).should.be.true;
                 ignition.ec2.request.restore();
+                callback();
             });
+        });        
+    });
+
+    describe('#ignition.getInstanceDetails', function() {
+        var validDescription = {
+            reservationSet: {
+                item: {
+                    instancesSet: {
+                        item: {
+                            instanceState: {
+                                name : "stopped"
+                            },
+                            tagSet: {
+                                item: [ { 
+                                    key: "Name",
+                                    value: "NAME"
+                                }]
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        beforeEach(function() {
+            var sampleResponse = {};
+            sinon.stub(ignition, 'describeInstances');
+            sinon.stub(ignition, 'getDisplayTextForState').returns('ACTION');
+        });
+        afterEach(function() {
+            ignition.describeInstances.restore();
+            ignition.getDisplayTextForState.restore();
+        });
+        it('should return unknowns on error', function(callback) {
+            ignition.describeInstances.yields('ERR');
+            ignition.getInstanceDetails('A', function(e, d) {
+                should.not.exist(e);
+                should.exist(d);
+                d.should.eql({ currentState: "unknown", action: "unknown", name: "unknown", launchtime: 'unknown' });
+                callback();
+            })
+        });
+        it('should return currentState when valid', function(callback) {
+            ignition.describeInstances.yields(null, validDescription);
+            ignition.getInstanceDetails('A', function(e, d) {
+                should.not.exist(e);
+                should.exist(d);
+                should.exist(d.currentState);
+                d.currentState.should.equal("stopped");
+                callback();
+            })
+        });
+        it('should return action when valid', function(callback) {
+            ignition.describeInstances.yields(null, validDescription);
+            ignition.getInstanceDetails('A', function(e, d) {
+                should.not.exist(e);
+                should.exist(d);
+                should.exist(d.action);
+                d.action.should.equal("ACTION");
+                callback();
+            })
+        });
+        it('should return name when valid', function(callback) {
+            ignition.describeInstances.yields(null, validDescription);
+            ignition.getInstanceDetails('A', function(e, d) {
+                should.not.exist(e);
+                should.exist(d);
+                should.exist(d.name);
+                d.name.should.equal("NAME");
+                callback();
+            })
+        });
+        it('should return launchtime when valid', function(callback) {
+            ignition.describeInstances.yields(null, validDescription);
+            ignition.getInstanceDetails('A', function(e, d) {
+                should.not.exist(e);
+                should.exist(d);
+                should.exist(d.launchtime);
+                d.launchtime.should.equal("unknown");
+                callback();
+            })
         });
     });
 });
